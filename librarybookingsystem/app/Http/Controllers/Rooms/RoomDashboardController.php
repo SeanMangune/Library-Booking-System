b4445c3de0e45292cd4faf598b5272dd0a0007bd
Albert Miguel Dizon <dizonalbertmiguelbarata@gmail.com>
2025-12-12 19:30:48 +0800

    added view render function

--- librarybookingsystem/app/Http/Controllers/Rooms/RoomDashboardController.php
+++ librarybookingsystem/app/Http/Controllers/Rooms/RoomDashboardController.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Http\Controllers\Rooms;
-
+use App\Models\Rooms;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class RoomDashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $rooms = Rooms::all();
+        return view('rooms.roomdashboard', compact('rooms'));
+    }
 }
