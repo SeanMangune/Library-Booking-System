e310bb33071fdad7ab7a86db16fc3b606f9b0b7b
Albert Miguel Dizon <dizonalbertmiguelbarata@gmail.com>
2025-12-12 19:30:01 +0800

    changed view route

--- librarybookingsystem/routes/web.php
+++ librarybookingsystem/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Rooms\RoomDashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RoomDashboardController::class, 'index']);
